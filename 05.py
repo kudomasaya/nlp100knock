58f40966f9479478212b090526ad51ce51676870
Masaya Kudo <m.kudo@favoritestar.co.jp>
2016-06-09 11:23:33 +0900

    05完了

--- 05.py
+++ 05.py
@@ -1,26 +1,26 @@
 # coding:utf-8
 
-def getNgram(seq, n):
+
+def get_ngram(seq, n):
     arr = []
     if isinstance(seq, str):
         arr = seq.split(" ")
     else:
         arr = list(seq)
 
-    #単語n-gram
-    #{I-am am-an an-NLPer}
-    res = []
+    # 単語n-gram
+    word_result = [arr[i:i+n] for i in range(0, len(arr))]
 
-    #文字n-gram
-    #{Ia,am,ma,an,nN,NL,LP,Pe,er,r}
+    # 文字n-gram
+    char_arr    = list(''.join(arr))
+    char_result = [char_arr[i:i+n] for i in range(0, len(char_arr))]
 
-    print arr
-    return {}
+    return {'word': word_result, 'char': char_result}
+
 
 def main():
-    #str = "I am an NLPer"
-    str = ["I","am","an","NLPer"]
-    res = getNgram(str, 2)
+    s = "I am an NLPer"
+    res = get_ngram(s, 2)
     print res
 
 if __name__ == '__main__':
