83b5f0ebc0b9bcbf4e2c249a07b61ed294260208
Masaya Kudo <m.kudo@favoritestar.co.jp>
2016-06-09 14:59:46 +0900

    05バグ修正＆07まで完了

--- 06.py
+++ 06.py
@@ -12,16 +12,25 @@ def get_ngram(seq, n):
     word_result = [arr[i:i+n] for i in range(0, len(arr))]
 
     # 文字n-gram
-    char_arr = list(''.join(arr))
+    char_arr    = list(''.join(arr))
     char_result = [''.join(char_arr[i:i+n]) for i in range(0, len(char_arr))]
 
     return {'word': word_result, 'char': char_result}
 
 
 def main():
-    s = "I am an NLPer"
-    res = get_ngram(s, 2)
-    print res
+    x = set(get_ngram("paraparaparadise", 2)['char'])
+    y = set(get_ngram("paragraph", 2)['char'])
+
+    print 'x = ' + str(x)
+    print 'y = ' + str(y)
+
+    print 'x | y = ' + str(x | y)
+    print 'x & y = ' + str(x & y)
+    print 'x - y = ' + str(x - y)
+
+    print 'x in se = ' + str('se' in x)
+    print 'y in se = ' + str('se' in y)
 
 if __name__ == '__main__':
     main()
---
+++ 07.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+
+
+def xyz(x, y, z):
+    return "{}時の{}は{}".format(x, y, z)
+
+def main():
+    print xyz(12, "気温", 22.4)
+
+if __name__ == '__main__':
+    main()
